03f3b8c4c2975ebc4107916926db6e152fd570e3
tanpt5 <tanpt5@fsoft.com.vn>
2023-01-06 15:01:37 +0700

    debug forceUpdate

--- src/modules/certificate/service/certificate.service.ts
+++ src/modules/certificate/service/certificate.service.ts
@@ -1,77 +1,15 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { X509Certificate } from 'crypto';
+import * as fs from 'fs';
 import { Repository } from 'typeorm';
 import { Node } from '../../node/entity/node.entity';
 import { Certificate } from '../entity/certificate.entity';
-import { ICertificateRes, IIssuedCertificate } from '../interfaces';
-import * as fs from 'fs';
 
 export class CertificateService {
   constructor(
     @InjectRepository(Certificate)
     private certificateRepository: Repository<Certificate>,
   ) {}
-
-  checkCertificateIssue(certificate: Buffer) {
-    const certificateData = new X509Certificate(certificate);
-    const expiredDay = certificateData.validTo;
-    const certExpiredDay = new Date(expiredDay);
-    const currentDate = new Date();
-    if (certExpiredDay < currentDate) return 'Certificate Expired';
-    return 'None';
-  }
-
-  async getAllCertificates(): Promise<ICertificateRes> {
-    return this.certificateRepository
-      .createQueryBuilder('certificate')
-      .innerJoin(Node, 'node', 'certificate.nodeId = node.id')
-      .select([
-        '"certificate"."id"',
-        '"certificate"."nodeId"',
-        '"node"."name"',
-        '"certificate"."expiredDay"',
-        '"certificate"."issuedDate"',
-        '"certificate"."isIssued"',
-        '"certificate"."createdAt"',
-        '"certificate"."updatedAt"',
-        '"certificate"."certificateIssue"',
-        'status',
-      ])
-      .orderBy('"node"."name"', 'ASC')
-      .execute() as Promise<ICertificateRes>;
-  }
-
-  async generateCertificateData(listNode: { id: string; name: string }[]) {
-    try {
-      await Promise.all([
-        listNode.map(async (item: { id: string; name: string }) => {
-          const isExist = await this.certificateRepository.findOne({
-            nodeId: item.id,
-          });
-          if (!isExist) {
-            await this.certificateRepository
-              .createQueryBuilder()
-              .insert()
-              .into(Certificate)
-              .values([
-                {
-                  nodeId: item.id,
-                  expiredDay: new Date(),
-                  issuedDate: new Date(),
-                  certificateIssue: 'No Certificate',
-                  isIssued: false,
-                },
-              ])
-              .execute();
-          }
-        }),
-      ]);
-      return { status: 'succeeded' };
-    } catch (err) {
-      console.log(err);
-      return { status: 'failed' };
-    }
-  }
 
   async insertCertificateData(certificate: Buffer) {
     try {
@@ -84,7 +22,9 @@ export class CertificateService {
       const currentDate = new Date();
 
       const certificateIssue =
-        certExpiredDay < currentDate ? 'Certificate Expired' : 'None';
+        certExpiredDay <= currentDate ? 'Certificate Expired' : 'None';
+      console.log('certificateIssue', certificateIssue);
+      console.log('certExpiredDay', certExpiredDay);
 
       const rootCA = new X509Certificate(fs.readFileSync('cert/ca-cert.pem'));
 
@@ -138,32 +78,6 @@ export class CertificateService {
     } catch (err) {
       console.log(err);
     }
-  }
-
-  async getExpiredCertificate() {
-    const expiredCertificate = await this.certificateRepository
-      .createQueryBuilder()
-      .select()
-      .where('"Certificate"."expiredDay" < Now()')
-      .getRawMany();
-
-    return {
-      expiredCertificate,
-      total: expiredCertificate.length || 0,
-    };
-  }
-
-  async getIssuedCertificate(): Promise<IIssuedCertificate> {
-    const issuedCertificate = await this.certificateRepository
-      .createQueryBuilder()
-      .select()
-      .where('"Certificate"."isIssued" = true')
-      .getRawMany();
-
-    return {
-      issuedCertificate,
-      total: issuedCertificate.length || 0,
-    };
   }
 
   async removeCertificate() {
